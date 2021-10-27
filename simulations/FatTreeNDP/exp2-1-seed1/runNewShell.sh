45bf1e5cdd1f1c0ac693af53bed3ec652cd2f85c
Aiden Valentine <aiden178@hotmail.co.uk>
2021-10-27 13:32:16 +0100

    Changed Experiments from Debug to Release

--- simulations/FatTreeNDP/exp2-1-seed1/runNewShell.sh
+++ simulations/FatTreeNDP/exp2-1-seed1/runNewShell.sh
@@ -1,6 +1,6 @@
 echo " Running FatTree Traffic .... "
 find . -name "*.vec" -type f -delete|find . -name  "*.sca"  -type f -delete| find . -name "*.vci" -type f -delete | find . -name "*.csv" -type f -delete
-opp_run_dbg -u Cmdenv -m -n ../..:../../../src:../../../../inet4/src:../../../../inet4/examples:../../../../inet4/tutorials:../../../../inet4/showcases -l ../../../src/ndp ../exp2-1-seed1.ini -c General -r "\$FatTreeSize==$(printf '%d' $1)  &&  \$numShortFlows==$(printf '%d' $2)"
+opp_run_release -u Cmdenv -m -n ../..:../../../src:../../../../inet4/src:../../../../inet4/examples:../../../../inet4/tutorials:../../../../inet4/showcases -l ../../../src/ndp ../exp2-1-seed1.ini -c General -r "\$FatTreeSize==$(printf '%d' $1)  &&  \$numShortFlows==$(printf '%d' $2)"
 
 echo "Throughput"
 scavetool export -T s -f "module(**.app[*]) AND ("mohThroughputNDP:last")"   -F CSV-S -o instThroughput.csv *.sca
